d3a45d9111cc02aa4e7d44b39dce6d3d5d544a7d
Ryan2144 <70672438+Ryan2144@users.noreply.github.com>
2021-01-31 15:50:45 -0600

    1207959552 - Multiples of 9

--- js/game_manager.js
+++ js/game_manager.js
@@ -4,7 +4,7 @@ function GameManager(size, InputManager, Actuator, ScoreManager) {
   this.scoreManager = new ScoreManager;
   this.actuator     = new Actuator;
 
-  this.startTiles   = 2;
+  this.startTiles   = 12;
 
   this.inputManager.on("move", this.move.bind(this));
   this.inputManager.on("restart", this.restart.bind(this));
@@ -59,7 +59,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 2 : 4;
+    var value = Math.random() < 0.96 ? 9 : 18;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
@@ -127,7 +127,7 @@ GameManager.prototype.move = function (direction) {
 
         // Only one merger per row traversal?
         if (next && next.value === tile.value && !next.mergedFrom) {
-          var multiply = Math.random() < 0.986 ? 2 : 2;
+          var multiply = Math.random() < 0.5 ? 2 : 2;
           var merged = new Tile(positions.next, tile.value * multiply);
           merged.mergedFrom = [tile, next];
 
@@ -140,8 +140,8 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value;
 
-          // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
+          // The mighty 1207959552 tile
+          if (merged.value === 1207959552) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
